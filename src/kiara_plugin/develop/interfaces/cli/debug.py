b6d1aac258ed2bdd9ac25eb29e86218d9381886c
Markus Binsteiner <markus@frkl.io>
2022-06-24 23:36:29 +0200

    chore: update template

--- src/kiara_plugin/develop/interfaces/cli/debug.py
+++ src/kiara_plugin/develop/interfaces/cli/debug.py
@@ -8,6 +8,7 @@
 import rich_click as click
 from kiara.context import Kiara
 from kiara.utils.cli import terminal_print_model
+from rich.table import Table
 
 
 @click.group("debug")
@@ -26,3 +27,20 @@ def print_jobs(ctx):
     all_records = kiara.job_registry.retrieve_all_job_records()
 
     terminal_print_model(*all_records.values())
+
+
+@debug.command("print-workflows")
+@click.pass_context
+def print_workflows(ctx):
+    """Print stored workflows."""
+
+    kiara: Kiara = ctx.obj["kiara"]
+
+    workflow_aliases = kiara.workflow_registry.workflow_aliases.keys()
+
+    table = Table(show_header=True)
+    table.add_column("workflow alias", style="i")
+    table.add_column("details")
+    for workflow_alias in workflow_aliases:
+        details = kiara.workflow_registry.get_workflow(workflow_alias)
+        print(details)
