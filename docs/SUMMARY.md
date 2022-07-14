96fd96eb2c6b41e58973b1752b7482637e16cc30
Markus Binsteiner <markus@frkl.io>
2022-07-14 13:36:30 +0200

    ci: remove --pre flag in all ci jobs

--- docs/SUMMARY.md
+++ docs/SUMMARY.md
@@ -1,4 +1,5 @@
 * [Home](index.md)
 * [Usage](usage.md)
 * [Package contents](info/)
+* [Development](development.md)
 * [API reference](reference/)
--- src/kiara_plugin/develop/__init__.py
+++ src/kiara_plugin/develop/__init__.py
@@ -36,7 +36,7 @@ KIARA_METADATA = {
 
 find_modules: KiaraEntryPointItem = (
     find_kiara_modules_under,
-    "kiara_plugin.develop.develop",
+    "kiara_plugin.develop.modules",
 )
 find_model_classes: KiaraEntryPointItem = (
     find_kiara_model_classes_under,
